89cfcb535a01cf463ca6a010cfd079d1cb7ea595
Arthur Jamet <arthur.jamet@gmail.com>
2025-12-25 11:33:19 +0100

    Nix: Dev: Install colima only on macos

--- modules/dev/default.nix
+++ modules/dev/default.nix
@@ -1,39 +1,46 @@
-{ pkgs, ... }:
 {
-  home.packages = with pkgs; [
-    # Haskell
-    haskell.compiler.ghc912
-    (haskell-language-server.override { supportedGhcVersions = [ "912" ]; })
-    haskellPackages.hlint
-    haskellPackages.fourmolu
-    haskellPackages.stack
+  pkgs,
+  lib,
+  isDarwin,
+  ...
+}:
 
-    gcc
-    bun
-    python313
+{
+  home.packages =
+    with pkgs;
+    [
+      # Haskell
+      haskell.compiler.ghc912
+      (haskell-language-server.override { supportedGhcVersions = [ "912" ]; })
+      haskellPackages.hlint
+      haskellPackages.fourmolu
+      haskellPackages.stack
 
-    colima
-    docker
-    docker-compose
+      gcc
+      bun
+      python313
 
-    yaml-language-server
+      docker
+      docker-compose
 
-    # Nix
-    nixfmt
-    # Latex
-    (texlive.combine {
-      inherit (texlive)
-        scheme-medium
-        biblatex
-        # from lipics template
-        multirow
-        threeparttable
-        comment
-        cleveref
-        tikz-qtree
-        import
-        ;
-    })
-  ];
+      yaml-language-server
 
+      # Nix
+      nixfmt
+      # Latex
+      (texlive.combine {
+        inherit (texlive)
+          scheme-medium
+          biblatex
+          # from lipics template
+          multirow
+          threeparttable
+          comment
+          cleveref
+          tikz-qtree
+          import
+          ;
+      })
+    ]
+    ++ lib.optionals isDarwin [ colima ];
 }
