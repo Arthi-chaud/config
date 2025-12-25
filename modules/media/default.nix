b7b938c3001aed0f6d473352172ec532b6eb1b2f
Arthur Jamet <arthur.jamet@gmail.com>
2025-12-25 11:54:40 +0100

    Nix: Media: Install makemkv using nixpkgs if not macos

--- modules/media/default.nix
+++ modules/media/default.nix
@@ -1,9 +1,16 @@
-{ pkgs, ... }:
 {
-  home.packages = with pkgs; [
-    # NOTE: Picard is not compatible with mac
-    ffmpeg
-    atomicparsley
-    yt-dlp
-  ];
+  pkgs,
+  isDarwin,
+  lib,
+  ...
+}:
+{
+  home.packages =
+    with pkgs;
+    [
+      ffmpeg
+      atomicparsley
+      yt-dlp
+    ]
+    ++ lib.optional (!isDarwin) [ makemkv ];
 }
