f181c244f68366c35dfddbb84cf1bad6b0086d89
Arthur Jamet <arthur.jamet@gmail.com>
2026-07-14 08:31:16 +0100

    Yabai: Use plain rc file + symlink

--- modules/yabai/default.nix
+++ modules/yabai/default.nix
@@ -1,5 +1,5 @@
 { home }:
-{ ... }:
+{ impurity, ... }:
 if home then
   {
     home.file."clear-empty-spaces.sh" = {
@@ -7,46 +7,15 @@ if home then
       source = ./clear-empty-spaces.sh;
       executable = true;
     };
+    home.file.".config/yabai/yabairc" = {
+      source = impurity.link ./yabairc;
+    };
   }
 else
-  let
-    padding = 10;
-  in
   {
     services.yabai = {
       enable = true;
       enableScriptingAddition = true;
-      config = {
-        mouse_follows_focus = "on";
-        layout = "bsp";
-        focus_follows_mouse = "autofocus";
-        window_border = "off";
-        external_bar = "all:35:0";
-        window_gap = padding;
-        top_padding = padding;
-        bottom_padding = padding;
-        left_padding = padding;
-        right_padding = padding;
-        # NOTE: does not work, had to set it up manually
-        window_shadow = "off";
-      };
-      extraConfig = ''
-        yabai -m signal --add event=dock_did_restart action="sudo yabai --load-sa"
-        sudo yabai --load-sa
-
-        yabai -m rule --add app="^(K|k)itty$" opacity=0.97
-        yabai -m rule --add app='qemu-system-aarch64' manage=off
-
-        # Clean up empty spaces with no windows on them.
-        yabai -m signal --add event=space_changed action="sh ~/.config/scripts/clear-empty-spaces.sh"
-        # Refocus on window destruction
-        for event in "window_destroyed" "application_terminated" "window_hidden" "window_minimized" "application_hidden";
-        do
-        yabai -m signal --add event="$event" action="yabai -m query --windows --window &> /dev/null || yabai -m window --focus mouse"
-        done
-
-        sleep 5 ; yabai -m config window_shadow off
-      '';
     };
 
   }
